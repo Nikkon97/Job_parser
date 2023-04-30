3a788c0529e30d9945245a73650c29c3fd1faddb
Nikkon97 <you@example.com>
2023-04-30 16:24:11 +0300

    На сдачу

--- src/global_variables.py
+++ src/global_variables.py
@@ -1,7 +1,7 @@
 PRICE_EUR = 85
 PRICE_USD = 80
-PAGE_COUNT = 2
+PAGE_COUNT = 10
 VACANCY_COUNT = 100
 
 # Необходимо указать свой ключ, для работы с superjob в str формате вместо ***
-key_job = "***"
+key_job = "v3.r.137520632.87cad08bd068b043d65e9ee792f1e655e51ab1ca.846de5c68f61c1b41eb7dff31899b1ad48f3eb52"
